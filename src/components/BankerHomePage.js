9ecf8e84a058de273e4d4fd3b3e00ca82e662a52
buba <bubajakhaia@gmail.com>
2024-07-28 23:12:49 +0400

    banker button

--- src/components/BankerHomePage.js
+++ src/components/BankerHomePage.js
@@ -16,7 +16,7 @@ const BankerHomePage = ({ isButton}) => {
       <h2 className='subscribe-header'>{language==="ge"?"პირადი ბანკირი":"Private Banker"}</h2>
       <p className='subscribe-text'>{language==="ge"?"თქვენი სანდო პარტნიორი ფინანსურ რჩევებსა და ოპერაციებში":"Your trusted partner in financial advice and operations"}</p>
       {isButton&& 
-      <Link to={`/products/personalbanker`}>
+      <Link to={`/ge/products/personalbanker`}>
         <button className='subscribe-button personal-banker-button'>
             <span className='subscribe-button-text'>{language==="ge"?"გაიგეთ მეტი":"LEARN MORE"}</span>
         </button>
